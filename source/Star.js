4b85b74d6f1713bfd86368461b171be5ba1bcec4
kPanic <hero.sungwoo@gmail.com>
2017-02-07 02:14:09 +0000

    view members who stared

--- source/Star.js
+++ source/Star.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {whoami, postThumbs, deleteThumbs, getThumbsByRock} from './actions'
+import {Tooltip, Position} from '@blueprintjs/core'
+import {getAllMembers, whoami, postThumbs, deleteThumbs, getThumbsByRock} from './actions'
 
 class Star extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ class Star extends Component {
     this.state = {
       isUp: false,
     }
+    this.props.getAllMembers()
     this.props.getThumbsByRock(this.props.rockId)
     .then( () => {
       this.props.whoami()
@@ -57,16 +59,26 @@ class Star extends Component {
   }
 
   render() {
+    const thumb = this.props.thumbs ? this.props.thumbs.filter(e => (e.rock && this.props.rockId == e.rock && e.isUp)) : false
+    const members = thumb && this.props.members.reduce((res, val) => (
+      thumb.map(e => e.member).includes(val.id) ? res.concat(val.last_name) : res), [])
+      .reduce((res, val) => res + val + "\n", "")
     return (
       <div>
         <p className="rock-star card-text text-xs-right text-muted">
           <small> 마음에 들어요{' '} </small>
-          <span 
-            onClick={this.toggleStar.bind(this)}
-            className={this.state.isUp ? "read-rock-star pt-icon pt-icon-star" : "read-rock-star pt-icon pt-icon-star-empty"}>
-            {this.props.thumbs && <small className="text-muted"> {this.props.thumbs.filter(e => (
-              e.rock && this.props.rockId == e.rock && e.isUp)).length} </small> }
-          </span>
+          <Tooltip
+            content={members && <span className="who-stared"> {members} </span>}
+            hoverOpenDelay={1}
+            hoverCloseDelay={1}
+            transitionDuration={1}
+            position={Position.RIGHT}>
+            <span 
+              onClick={this.toggleStar.bind(this)}
+              className={this.state.isUp ? "read-rock-star pt-icon pt-icon-star" : "read-rock-star pt-icon pt-icon-star-empty"}>
+              {thumb && <small className="text-muted"> {thumb.length} </small>}
+            </span>
+          </Tooltip>
         </p>
       </div>
     )
@@ -79,6 +91,7 @@ Star.contextTypes = {
 
 const mapStateToProps = state => ({
   thumbs: state.thumbs,
+  members: state.members,
   me: state.me
 })
 
@@ -86,6 +99,7 @@ const mapDispatchToProps = ({
   getThumbsByRock,
   postThumbs,
   whoami,
+  getAllMembers,
   deleteThumbs,
 })
 
